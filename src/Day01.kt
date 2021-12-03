fc0616ceb75db596d5fc81dbc469e462112dacb6
Patrick <patrick@pmueller.eu>
2021-12-03 14:24:35 +0100

    Add first Day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { Integer.valueOf(it) }.zipWithNext().fold(0) { acc, (first,second) ->
+            if (first < second) acc + 1 else acc
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map {Integer.valueOf(it) }.windowed(3).zipWithNext().fold(0) { acc, (first,second) ->
+            if (first[0] < second[2]) acc + 1 else acc
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("day1")
     println(part1(input))
     println(part2(input))
 }
